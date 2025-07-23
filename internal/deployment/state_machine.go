9679cababa13871b9ffa0315a0d709e665381da1
Roman Heinrich <roman.heinrich@gmail.com>
2025-07-23 19:58:31 +0200

    Feat: state machine for deployment

---
+++ internal/deployment/state_machine.go
@@ -0,0 +1,347 @@
+package deployment
+
+import (
+	"fmt"
+	"time"
+)
+
+type DeploymentState string
+
+const (
+	StateStable      DeploymentState = "stable"
+	StateStarting    DeploymentState = "starting"
+	StateHealthCheck DeploymentState = "health_check"
+	StateSwitching   DeploymentState = "switching"
+	StateDraining    DeploymentState = "draining"
+	StateStopping    DeploymentState = "stopping"
+	StateRollingBack DeploymentState = "rolling_back"
+	StateFailed      DeploymentState = "failed"
+)
+
+type DeploymentEvent string
+
+const (
+	EventDeploy            DeploymentEvent = "deploy"
+	EventContainerStarted  DeploymentEvent = "container_started"
+	EventContainerFailed   DeploymentEvent = "container_failed"
+	EventHealthCheckPassed DeploymentEvent = "health_check_passed"
+	EventHealthCheckFailed DeploymentEvent = "health_check_failed"
+	EventCaddyUpdated      DeploymentEvent = "caddy_updated"
+	EventCaddyFailed       DeploymentEvent = "caddy_failed"
+	EventDrainComplete     DeploymentEvent = "drain_complete"
+	EventContainerStopped  DeploymentEvent = "container_stopped"
+	EventStopFailed        DeploymentEvent = "stop_failed"
+	EventRollbackComplete  DeploymentEvent = "rollback_complete"
+	EventRollbackFailed    DeploymentEvent = "rollback_failed"
+	EventManualRecovery    DeploymentEvent = "manual_recovery"
+)
+
+type ActionProvider interface {
+	StartContainer(appName, color, image string) error
+	CheckHealth(appName, color string) (bool, error)
+	UpdateCaddy(appName, activeColor string) error
+	DrainConnections(appName, color string, timeout time.Duration) error
+	StopContainer(appName, color string) error
+	RollbackCaddy(appName, activeColor string) error
+}
+
+type DeploymentStateMachine struct {
+	state         DeploymentState
+	appName       string
+	newImage      string
+	activeColor   string
+	targetColor   string
+	previousColor string
+	actions       ActionProvider
+
+	healthCheckTimeout time.Duration
+	drainTimeout       time.Duration
+
+	stateHistory []StateTransition
+}
+
+type StateTransition struct {
+	FromState DeploymentState
+	ToState   DeploymentState
+	Event     DeploymentEvent
+	Timestamp time.Time
+	Error     error
+}
+
+func New(appName, activeColor string, actions ActionProvider) *DeploymentStateMachine {
+	return &DeploymentStateMachine{
+		state:              StateStable,
+		appName:            appName,
+		activeColor:        activeColor,
+		actions:            actions,
+		healthCheckTimeout: 60 * time.Second,
+		drainTimeout:       30 * time.Second,
+		stateHistory:       make([]StateTransition, 0),
+	}
+}
+
+func (sm *DeploymentStateMachine) GetState() DeploymentState {
+	return sm.state
+}
+
+func (sm *DeploymentStateMachine) GetStateHistory() []StateTransition {
+	return sm.stateHistory
+}
+
+func (sm *DeploymentStateMachine) SetHealthCheckTimeout(timeout time.Duration) {
+	sm.healthCheckTimeout = timeout
+}
+
+func (sm *DeploymentStateMachine) SetDrainTimeout(timeout time.Duration) {
+	sm.drainTimeout = timeout
+}
+
+func (sm *DeploymentStateMachine) ProcessEvent(event DeploymentEvent) error {
+	oldState := sm.state
+	var err error
+
+	switch sm.state {
+	case StateStable:
+		err = sm.handleStableState(event)
+	case StateStarting:
+		err = sm.handleStartingState(event)
+	case StateHealthCheck:
+		err = sm.handleHealthCheckState(event)
+	case StateSwitching:
+		err = sm.handleSwitchingState(event)
+	case StateDraining:
+		err = sm.handleDrainingState(event)
+	case StateStopping:
+		err = sm.handleStoppingState(event)
+	case StateRollingBack:
+		err = sm.handleRollingBackState(event)
+	case StateFailed:
+		err = sm.handleFailedState(event)
+	default:
+		return fmt.Errorf("unknown state: %s", sm.state)
+	}
+
+	sm.recordTransition(oldState, sm.state, event, err)
+	return err
+}
+
+func (sm *DeploymentStateMachine) Deploy(newImage string) error {
+	if sm.state != StateStable {
+		return fmt.Errorf("cannot deploy in state %s, must be in stable state", sm.state)
+	}
+
+	sm.newImage = newImage
+	sm.targetColor = sm.getInactiveColor()
+	sm.previousColor = sm.activeColor
+
+	return sm.ProcessEvent(EventDeploy)
+}
+
+func (sm *DeploymentStateMachine) handleStableState(event DeploymentEvent) error {
+	switch event {
+	case EventDeploy:
+		sm.state = StateStarting
+		return sm.actions.StartContainer(sm.appName, sm.targetColor, sm.newImage)
+	case EventManualRecovery:
+		// Already stable, no-op
+		return nil
+	default:
+		return fmt.Errorf("invalid event %s for state %s", event, sm.state)
+	}
+}
+
+func (sm *DeploymentStateMachine) handleStartingState(event DeploymentEvent) error {
+	switch event {
+	case EventContainerStarted:
+		sm.state = StateHealthCheck
+		return nil
+	case EventContainerFailed:
+		sm.state = StateFailed
+		return fmt.Errorf("container failed to start")
+	default:
+		return fmt.Errorf("invalid event %s for state %s", event, sm.state)
+	}
+}
+
+func (sm *DeploymentStateMachine) handleHealthCheckState(event DeploymentEvent) error {
+	switch event {
+	case EventHealthCheckPassed:
+		sm.state = StateSwitching
+		return sm.actions.UpdateCaddy(sm.appName, sm.targetColor)
+	case EventHealthCheckFailed:
+		sm.state = StateRollingBack
+		return sm.actions.StopContainer(sm.appName, sm.targetColor)
+	default:
+		return fmt.Errorf("invalid event %s for state %s", event, sm.state)
+	}
+}
+
+func (sm *DeploymentStateMachine) handleSwitchingState(event DeploymentEvent) error {
+	switch event {
+	case EventCaddyUpdated:
+		sm.state = StateDraining
+		return sm.actions.DrainConnections(sm.appName, sm.previousColor, sm.drainTimeout)
+	case EventCaddyFailed:
+		sm.state = StateRollingBack
+		return sm.actions.StopContainer(sm.appName, sm.targetColor)
+	default:
+		return fmt.Errorf("invalid event %s for state %s", event, sm.state)
+	}
+}
+
+func (sm *DeploymentStateMachine) handleDrainingState(event DeploymentEvent) error {
+	switch event {
+	case EventDrainComplete:
+		sm.state = StateStopping
+		return sm.actions.StopContainer(sm.appName, sm.previousColor)
+	default:
+		return fmt.Errorf("invalid event %s for state %s", event, sm.state)
+	}
+}
+
+func (sm *DeploymentStateMachine) handleStoppingState(event DeploymentEvent) error {
+	switch event {
+	case EventContainerStopped:
+		sm.state = StateStable
+		sm.activeColor = sm.targetColor
+		return nil
+	case EventStopFailed:
+		sm.state = StateFailed
+		return fmt.Errorf("failed to stop old container")
+	default:
+		return fmt.Errorf("invalid event %s for state %s", event, sm.state)
+	}
+}
+
+func (sm *DeploymentStateMachine) handleRollingBackState(event DeploymentEvent) error {
+	switch event {
+	case EventRollbackComplete:
+		sm.state = StateStable
+		return nil
+	case EventRollbackFailed:
+		sm.state = StateFailed
+		return fmt.Errorf("rollback failed")
+	default:
+		return fmt.Errorf("invalid event %s for state %s", event, sm.state)
+	}
+}
+
+func (sm *DeploymentStateMachine) handleFailedState(event DeploymentEvent) error {
+	switch event {
+	case EventManualRecovery:
+		sm.state = StateStable
+		return nil
+	default:
+		return fmt.Errorf("invalid event %s for state %s", event, sm.state)
+	}
+}
+
+func (sm *DeploymentStateMachine) CheckHealth() error {
+	if sm.state != StateHealthCheck {
+		return fmt.Errorf("cannot check health in state %s", sm.state)
+	}
+
+	healthy, err := sm.actions.CheckHealth(sm.appName, sm.targetColor)
+	if err != nil {
+		return sm.ProcessEvent(EventHealthCheckFailed)
+	}
+
+	if healthy {
+		return sm.ProcessEvent(EventHealthCheckPassed)
+	}
+
+	return nil // Still checking
+}
+
+func (sm *DeploymentStateMachine) CompleteHealthCheck(passed bool) error {
+	if passed {
+		return sm.ProcessEvent(EventHealthCheckPassed)
+	} else {
+		return sm.ProcessEvent(EventHealthCheckFailed)
+	}
+}
+
+func (sm *DeploymentStateMachine) CompleteDrain() error {
+	return sm.ProcessEvent(EventDrainComplete)
+}
+
+func (sm *DeploymentStateMachine) CompleteContainerOperation(success bool, isRollback bool) error {
+	if isRollback {
+		if success {
+			return sm.ProcessEvent(EventRollbackComplete)
+		} else {
+			return sm.ProcessEvent(EventRollbackFailed)
+		}
+	}
+
+	switch sm.state {
+	case StateStarting:
+		if success {
+			return sm.ProcessEvent(EventContainerStarted)
+		} else {
+			return sm.ProcessEvent(EventContainerFailed)
+		}
+	case StateStopping:
+		if success {
+			return sm.ProcessEvent(EventContainerStopped)
+		} else {
+			return sm.ProcessEvent(EventStopFailed)
+		}
+	default:
+		return fmt.Errorf("unexpected container operation completion in state %s", sm.state)
+	}
+}
+
+func (sm *DeploymentStateMachine) CompleteCaddyUpdate(success bool) error {
+	if success {
+		return sm.ProcessEvent(EventCaddyUpdated)
+	} else {
+		return sm.ProcessEvent(EventCaddyFailed)
+	}
+}
+
+func (sm *DeploymentStateMachine) RecoverManually() error {
+	return sm.ProcessEvent(EventManualRecovery)
+}
+
+func (sm *DeploymentStateMachine) getInactiveColor() string {
+	if sm.activeColor == "blue" {
+		return "green"
+	}
+	return "blue"
+}
+
+func (sm *DeploymentStateMachine) recordTransition(fromState, toState DeploymentState, event DeploymentEvent, err error) {
+	transition := StateTransition{
+		FromState: fromState,
+		ToState:   toState,
+		Event:     event,
+		Timestamp: time.Now(),
+		Error:     err,
+	}
+	sm.stateHistory = append(sm.stateHistory, transition)
+}
+
+func (sm *DeploymentStateMachine) GetActiveColor() string {
+	return sm.activeColor
+}
+
+func (sm *DeploymentStateMachine) GetTargetColor() string {
+	return sm.targetColor
+}
+
+func (sm *DeploymentStateMachine) GetNewImage() string {
+	return sm.newImage
+}
+
+func (sm *DeploymentStateMachine) IsInProgress() bool {
+	return sm.state != StateStable && sm.state != StateFailed
+}
+
+func (sm *DeploymentStateMachine) CanDeploy() bool {
+	return sm.state == StateStable
+}
+
+func (sm *DeploymentStateMachine) NeedsManualIntervention() bool {
+	return sm.state == StateFailed
+}
---
+++ internal/deployment/state_machine_test.go
@@ -0,0 +1,436 @@
+package deployment
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+type MockActionProvider struct {
+	startContainerError   error
+	checkHealthResult     bool
+	checkHealthError      error
+	updateCaddyError      error
+	drainConnectionsError error
+	stopContainerError    error
+	rollbackCaddyError    error
+
+	// Call tracking
+	startContainerCalls   []StartContainerCall
+	checkHealthCalls      []CheckHealthCall
+	updateCaddyCalls      []UpdateCaddyCall
+	drainConnectionsCalls []DrainConnectionsCall
+	stopContainerCalls    []StopContainerCall
+	rollbackCaddyCalls    []RollbackCaddyCall
+}
+
+type StartContainerCall struct {
+	AppName string
+	Color   string
+	Image   string
+}
+
+type CheckHealthCall struct {
+	AppName string
+	Color   string
+}
+
+type UpdateCaddyCall struct {
+	AppName     string
+	ActiveColor string
+}
+
+type DrainConnectionsCall struct {
+	AppName string
+	Color   string
+	Timeout time.Duration
+}
+
+type StopContainerCall struct {
+	AppName string
+	Color   string
+}
+
+type RollbackCaddyCall struct {
+	AppName     string
+	ActiveColor string
+}
+
+func (m *MockActionProvider) StartContainer(appName, color, image string) error {
+	m.startContainerCalls = append(m.startContainerCalls, StartContainerCall{
+		AppName: appName,
+		Color:   color,
+		Image:   image,
+	})
+	return m.startContainerError
+}
+
+func (m *MockActionProvider) CheckHealth(appName, color string) (bool, error) {
+	m.checkHealthCalls = append(m.checkHealthCalls, CheckHealthCall{
+		AppName: appName,
+		Color:   color,
+	})
+	return m.checkHealthResult, m.checkHealthError
+}
+
+func (m *MockActionProvider) UpdateCaddy(appName, activeColor string) error {
+	m.updateCaddyCalls = append(m.updateCaddyCalls, UpdateCaddyCall{
+		AppName:     appName,
+		ActiveColor: activeColor,
+	})
+	return m.updateCaddyError
+}
+
+func (m *MockActionProvider) DrainConnections(appName, color string, timeout time.Duration) error {
+	m.drainConnectionsCalls = append(m.drainConnectionsCalls, DrainConnectionsCall{
+		AppName: appName,
+		Color:   color,
+		Timeout: timeout,
+	})
+	return m.drainConnectionsError
+}
+
+func (m *MockActionProvider) StopContainer(appName, color string) error {
+	m.stopContainerCalls = append(m.stopContainerCalls, StopContainerCall{
+		AppName: appName,
+		Color:   color,
+	})
+	return m.stopContainerError
+}
+
+func (m *MockActionProvider) RollbackCaddy(appName, activeColor string) error {
+	m.rollbackCaddyCalls = append(m.rollbackCaddyCalls, RollbackCaddyCall{
+		AppName:     appName,
+		ActiveColor: activeColor,
+	})
+	return m.rollbackCaddyError
+}
+
+func TestNew(t *testing.T) {
+	actions := &MockActionProvider{}
+	sm := New("test-app", "blue", actions)
+
+	if sm.GetState() != StateStable {
+		t.Errorf("New() initial state = %v, want %v", sm.GetState(), StateStable)
+	}
+	if sm.GetActiveColor() != "blue" {
+		t.Errorf("New() active color = %v, want %v", sm.GetActiveColor(), "blue")
+	}
+	if sm.appName != "test-app" {
+		t.Errorf("New() app name = %v, want %v", sm.appName, "test-app")
+	}
+}
+
+func TestSuccessfulDeployment(t *testing.T) {
+	actions := &MockActionProvider{}
+	sm := New("test-app", "blue", actions)
+
+	// Start deployment
+	err := sm.Deploy("nginx:1.22")
+	if err != nil {
+		t.Fatalf("Deploy() failed: %v", err)
+	}
+
+	// Verify state transition and action call
+	if sm.GetState() != StateStarting {
+		t.Errorf("After Deploy() state = %v, want %v", sm.GetState(), StateStarting)
+	}
+	if len(actions.startContainerCalls) != 1 {
+		t.Errorf("StartContainer called %d times, want 1", len(actions.startContainerCalls))
+	}
+	if actions.startContainerCalls[0].Color != "green" {
+		t.Errorf("StartContainer color = %v, want green", actions.startContainerCalls[0].Color)
+	}
+
+	// Container started successfully
+	err = sm.CompleteContainerOperation(true, false)
+	if err != nil {
+		t.Fatalf("CompleteContainerOperation() failed: %v", err)
+	}
+	if sm.GetState() != StateHealthCheck {
+		t.Errorf("After container started state = %v, want %v", sm.GetState(), StateHealthCheck)
+	}
+
+	// Health check passed
+	err = sm.CompleteHealthCheck(true)
+	if err != nil {
+		t.Fatalf("CompleteHealthCheck() failed: %v", err)
+	}
+	if sm.GetState() != StateSwitching {
+		t.Errorf("After health check state = %v, want %v", sm.GetState(), StateSwitching)
+	}
+	if len(actions.updateCaddyCalls) != 1 {
+		t.Errorf("UpdateCaddy called %d times, want 1", len(actions.updateCaddyCalls))
+	}
+
+	// Caddy updated successfully
+	err = sm.CompleteCaddyUpdate(true)
+	if err != nil {
+		t.Fatalf("CompleteCaddyUpdate() failed: %v", err)
+	}
+	if sm.GetState() != StateDraining {
+		t.Errorf("After Caddy update state = %v, want %v", sm.GetState(), StateDraining)
+	}
+	if len(actions.drainConnectionsCalls) != 1 {
+		t.Errorf("DrainConnections called %d times, want 1", len(actions.drainConnectionsCalls))
+	}
+
+	// Drain completed
+	err = sm.CompleteDrain()
+	if err != nil {
+		t.Fatalf("CompleteDrain() failed: %v", err)
+	}
+	if sm.GetState() != StateStopping {
+		t.Errorf("After drain complete state = %v, want %v", sm.GetState(), StateStopping)
+	}
+	if len(actions.stopContainerCalls) != 1 {
+		t.Errorf("StopContainer called %d times, want 1", len(actions.stopContainerCalls))
+	}
+	if actions.stopContainerCalls[0].Color != "blue" {
+		t.Errorf("StopContainer color = %v, want blue", actions.stopContainerCalls[0].Color)
+	}
+
+	// Old container stopped
+	err = sm.CompleteContainerOperation(true, false)
+	if err != nil {
+		t.Fatalf("CompleteContainerOperation() failed: %v", err)
+	}
+	if sm.GetState() != StateStable {
+		t.Errorf("After container stopped state = %v, want %v", sm.GetState(), StateStable)
+	}
+	if sm.GetActiveColor() != "green" {
+		t.Errorf("After deployment active color = %v, want green", sm.GetActiveColor())
+	}
+}
+
+func TestContainerStartFailure(t *testing.T) {
+	actions := &MockActionProvider{
+		startContainerError: fmt.Errorf("container start failed"),
+	}
+	sm := New("test-app", "blue", actions)
+
+	// Start deployment
+	err := sm.Deploy("nginx:1.22")
+	if err == nil {
+		t.Errorf("Deploy() should fail when container start fails")
+	}
+	if sm.GetState() != StateStarting {
+		t.Errorf("After failed Deploy() state = %v, want %v", sm.GetState(), StateStarting)
+	}
+
+	// Container failed to start
+	err = sm.CompleteContainerOperation(false, false)
+	if err == nil {
+		t.Errorf("CompleteContainerOperation() should return error for failure")
+	}
+	if sm.GetState() != StateFailed {
+		t.Errorf("After container start failure state = %v, want %v", sm.GetState(), StateFailed)
+	}
+}
+
+func TestHealthCheckFailureRollback(t *testing.T) {
+	actions := &MockActionProvider{}
+	sm := New("test-app", "blue", actions)
+
+	// Get to health check state
+	sm.Deploy("nginx:1.22")
+	sm.CompleteContainerOperation(true, false)
+
+	// Health check failed
+	err := sm.CompleteHealthCheck(false)
+	if err != nil {
+		t.Fatalf("CompleteHealthCheck() failed: %v", err)
+	}
+	if sm.GetState() != StateRollingBack {
+		t.Errorf("After health check failure state = %v, want %v", sm.GetState(), StateRollingBack)
+	}
+
+	// Verify rollback action (stop new container)
+	if len(actions.stopContainerCalls) != 1 {
+		t.Errorf("StopContainer called %d times during rollback, want 1", len(actions.stopContainerCalls))
+	}
+	if actions.stopContainerCalls[0].Color != "green" {
+		t.Errorf("Rollback StopContainer color = %v, want green", actions.stopContainerCalls[0].Color)
+	}
+
+	// Complete rollback
+	err = sm.CompleteContainerOperation(true, true)
+	if err != nil {
+		t.Fatalf("CompleteContainerOperation() rollback failed: %v", err)
+	}
+	if sm.GetState() != StateStable {
+		t.Errorf("After rollback state = %v, want %v", sm.GetState(), StateStable)
+	}
+	if sm.GetActiveColor() != "blue" {
+		t.Errorf("After rollback active color = %v, want blue", sm.GetActiveColor())
+	}
+}
+
+func TestCaddyUpdateFailureRollback(t *testing.T) {
+	actions := &MockActionProvider{
+		updateCaddyError: fmt.Errorf("caddy update failed"),
+	}
+	sm := New("test-app", "blue", actions)
+
+	// Get to switching state
+	sm.Deploy("nginx:1.22")
+	sm.CompleteContainerOperation(true, false)
+	sm.CompleteHealthCheck(true)
+
+	// Caddy update failed
+	err := sm.CompleteCaddyUpdate(false)
+	if err != nil {
+		t.Fatalf("CompleteCaddyUpdate() failed: %v", err)
+	}
+	if sm.GetState() != StateRollingBack {
+		t.Errorf("After Caddy failure state = %v, want %v", sm.GetState(), StateRollingBack)
+	}
+}
+
+func TestInvalidStateTransitions(t *testing.T) {
+	actions := &MockActionProvider{}
+	sm := New("test-app", "blue", actions)
+
+	tests := []struct {
+		name         string
+		currentState DeploymentState
+		event        DeploymentEvent
+		shouldError  bool
+	}{
+		{"Deploy from non-stable", StateStarting, EventDeploy, true},
+		{"Invalid event in stable", StateStable, EventContainerStarted, true},
+		{"Invalid event in starting", StateStarting, EventHealthCheckPassed, true},
+		{"Invalid event in health check", StateHealthCheck, EventDrainComplete, true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sm.state = tt.currentState
+			err := sm.ProcessEvent(tt.event)
+			if tt.shouldError && err == nil {
+				t.Errorf("ProcessEvent() should fail for invalid transition")
+			} else if !tt.shouldError && err != nil {
+				t.Errorf("ProcessEvent() should not fail: %v", err)
+			}
+		})
+	}
+}
+
+func TestStateHistory(t *testing.T) {
+	actions := &MockActionProvider{}
+	sm := New("test-app", "blue", actions)
+
+	// Perform some state transitions
+	sm.Deploy("nginx:1.22")
+	sm.CompleteContainerOperation(true, false)
+
+	history := sm.GetStateHistory()
+	if len(history) != 2 {
+		t.Errorf("State history length = %d, want 2", len(history))
+	}
+
+	// Check first transition
+	if history[0].FromState != StateStable || history[0].ToState != StateStarting {
+		t.Errorf("First transition = %v -> %v, want %v -> %v",
+			history[0].FromState, history[0].ToState, StateStable, StateStarting)
+	}
+	if history[0].Event != EventDeploy {
+		t.Errorf("First transition event = %v, want %v", history[0].Event, EventDeploy)
+	}
+
+	// Check second transition
+	if history[1].FromState != StateStarting || history[1].ToState != StateHealthCheck {
+		t.Errorf("Second transition = %v -> %v, want %v -> %v",
+			history[1].FromState, history[1].ToState, StateStarting, StateHealthCheck)
+	}
+}
+
+func TestManualRecovery(t *testing.T) {
+	actions := &MockActionProvider{}
+	sm := New("test-app", "blue", actions)
+
+	// Force into failed state
+	sm.state = StateFailed
+
+	err := sm.RecoverManually()
+	if err != nil {
+		t.Errorf("RecoverManually() failed: %v", err)
+	}
+	if sm.GetState() != StateStable {
+		t.Errorf("After manual recovery state = %v, want %v", sm.GetState(), StateStable)
+	}
+}
+
+func TestTimeoutConfiguration(t *testing.T) {
+	actions := &MockActionProvider{}
+	sm := New("test-app", "blue", actions)
+
+	healthTimeout := 120 * time.Second
+	drainTimeout := 45 * time.Second
+
+	sm.SetHealthCheckTimeout(healthTimeout)
+	sm.SetDrainTimeout(drainTimeout)
+
+	if sm.healthCheckTimeout != healthTimeout {
+		t.Errorf("Health check timeout = %v, want %v", sm.healthCheckTimeout, healthTimeout)
+	}
+	if sm.drainTimeout != drainTimeout {
+		t.Errorf("Drain timeout = %v, want %v", sm.drainTimeout, drainTimeout)
+	}
+}
+
+func TestHelperMethods(t *testing.T) {
+	actions := &MockActionProvider{}
+	sm := New("test-app", "blue", actions)
+
+	// Test initial state
+	if !sm.CanDeploy() {
+		t.Errorf("CanDeploy() should be true in stable state")
+	}
+	if sm.IsInProgress() {
+		t.Errorf("IsInProgress() should be false in stable state")
+	}
+	if sm.NeedsManualIntervention() {
+		t.Errorf("NeedsManualIntervention() should be false in stable state")
+	}
+
+	// Start deployment
+	sm.Deploy("nginx:1.22")
+
+	if sm.CanDeploy() {
+		t.Errorf("CanDeploy() should be false during deployment")
+	}
+	if !sm.IsInProgress() {
+		t.Errorf("IsInProgress() should be true during deployment")
+	}
+	if sm.NeedsManualIntervention() {
+		t.Errorf("NeedsManualIntervention() should be false during normal deployment")
+	}
+
+	// Force failed state
+	sm.state = StateFailed
+
+	if sm.CanDeploy() {
+		t.Errorf("CanDeploy() should be false in failed state")
+	}
+	if sm.IsInProgress() {
+		t.Errorf("IsInProgress() should be false in failed state")
+	}
+	if !sm.NeedsManualIntervention() {
+		t.Errorf("NeedsManualIntervention() should be true in failed state")
+	}
+}
+
+func TestGetInactiveColor(t *testing.T) {
+	actions := &MockActionProvider{}
+
+	blueActiveSm := New("test-app", "blue", actions)
+	if blueActiveSm.getInactiveColor() != "green" {
+		t.Errorf("Inactive color for blue active = %v, want green", blueActiveSm.getInactiveColor())
+	}
+
+	greenActiveSm := New("test-app", "green", actions)
+	if greenActiveSm.getInactiveColor() != "blue" {
+		t.Errorf("Inactive color for green active = %v, want blue", greenActiveSm.getInactiveColor())
+	}
+}
